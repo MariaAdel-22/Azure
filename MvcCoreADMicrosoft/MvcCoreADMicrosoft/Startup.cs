0034d8ab4facec8aad23557f204130ffdd4fb005
MariaAdel-22 <maria.espana1@educamadrid.org>
2022-03-02 14:14:56 +0100

    com6

---
+++ MvcCoreADMicrosoft/MvcCoreADMicrosoft/Startup.cs
@@ -0,0 +1,78 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MvcCoreADMicrosoft.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreADMicrosoft
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string cadena = this.Configuration.GetConnectionString("cadenaAspNetDb");
+            string appid = this.Configuration.GetValue<string>("Authentication:Microsoft:ApplicationId");
+            string secretkey = this.Configuration.GetValue<string>("Authentication:Microsoft:SecretKey");
+
+            services.AddDbContext<ApplicationDbContext>(op => op.UseSqlServer(cadena));
+
+            //Decimos que tenemos un usuario de terceros pero administrado por nuestro context
+            services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+
+            //habilitar proveedores 
+            services.AddAuthentication().AddMicrosoftAccount(options =>
+            {
+                options.ClientId = appid;
+                options.ClientSecret = secretkey;
+            });
+
+            services.AddControllersWithViews(op => op.EnableEndpointRouting = false);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}"
+                );
+            });
+        }
+    }
+}
