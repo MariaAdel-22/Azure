b710978399f869a791813483bc2bc05eb2dc8e8c
MariaAdel-22 <maria.espana1@educamadrid.org>
2022-03-08 10:03:53 +0100

    com1

---
+++ ApiHospitales/ApiHospitales/Repositories/RepositoryHospital.cs
@@ -0,0 +1,29 @@
+﻿using ApiHospitales.Data;
+using ApiHospitales.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiHospitales.Repositories
+{
+    public class RepositoryHospital
+    {
+        private HospitalContext context;
+
+        public RepositoryHospital(HospitalContext context) {
+
+            this.context = context;
+        }
+
+        public List<Hospital> GetHospitales() {
+
+            return this.context.Hospitales.ToList();
+        }
+
+        public Hospital FindHospital(int id) {
+
+            return this.context.Hospitales.FirstOrDefault(z => z.IdHospital == id);
+        }
+    }
+}
---
+++ MvcClienteHospitalApi/MvcClienteHospitalApi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcClienteHospitalApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
