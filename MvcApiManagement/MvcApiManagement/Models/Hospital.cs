7f6413550c9363f5ab84a1a8a809ff7f5951b7ad
MariaAdel-22 <maria.espana1@educamadrid.org>
2022-03-11 12:51:37 +0100

    com4

---
+++ MvcApiManagement/MvcApiManagement/Models/Hospital.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcApiManagement.Models
+{
+    public class Hospital
+    {
+        public string IdHospital { get; set; }
+        public string Nombre { get; set; }
+
+        public string Direccion { get; set; }
+
+        public string Telefono { get; set; }
+
+        public int Camas { get; set; }
+    }
+}
---
+++ MvcApiManagement/MvcApiManagement/Services/ServiceApiHospital.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using MvcApiManagement.Models;
+using System.Net.Http;
+using System.Web;
+
+namespace MvcApiManagement.Services
+{
+    public class ServiceApiHospital
+    {
+        private string UrlApi;
+        private MediaTypeWithQualityHeaderValue Header;
+
+        public ServiceApiHospital(string url) {
+
+            this.UrlApi = url;
+            this.Header = new MediaTypeWithQualityHeaderValue("application/json");
+        }
+
+        public async Task<List<Hospital>> GetHospitales() {
+
+            using (HttpClient client = new HttpClient()) {
+
+                //Las peticiones API MANAGEMENT INCLUYEN UN QUERY STRING VACIO AL FINAL DE LA PETICION
+                var queryString = HttpUtility.ParseQueryString(string.Empty); 
+
+                var request = "/api/hospitales?"+queryString;
+
+                //LAS PETICIONES A API MANAGEMENT NO TIENEN BASE ADDRESS
+                
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+
+                //DEBEMOS INDICAR EN LAS CABECERAS(HEADERS) QUE NO UTILICE CACHE
+                client.DefaultRequestHeaders.CacheControl = CacheControlHeaderValue.Parse("no-cache");
+
+                //LAS PETICIONES SE REALIZAN EN EL PROPIO METODO DE LLAMADA JUNTO A LA URL
+                HttpResponseMessage response = await client.GetAsync(this.UrlApi + request);
+
+                if (response.IsSuccessStatusCode)
+                {
+
+                    List<Hospital> hospitales = await response.Content.ReadAsAsync<List<Hospital>>();
+                    return hospitales;
+                }
+                else {
+
+                    return null;
+                }
+            }
+        }
+    }
+}
