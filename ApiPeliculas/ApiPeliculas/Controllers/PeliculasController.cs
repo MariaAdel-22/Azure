662d65d0a45f0ca9996d9fdf412f0b3f7c63b946
MariaAdel-22 <maria.espana1@educa.madrid.org>
2022-03-16 19:15:38 +0100

    com3

---
+++ ApiPeliculas/ApiPeliculas/Controllers/PeliculasController.cs
@@ -0,0 +1,84 @@
+﻿using ApiPeliculas.Models;
+using ApiPeliculas.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPeliculas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeliculasController : ControllerBase
+    {
+
+        private RepositoryPeliculas repo;
+
+        public PeliculasController(RepositoryPeliculas repo) {
+
+            this.repo = repo;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Pelicula>> GetPeliculas() {
+
+            return this.repo.GetPeliculas();
+        }
+
+        [HttpGet]
+        [Route("[action]/{id}")]
+        public ActionResult<Pelicula> FindPeliculaId(int id) {
+
+            return this.repo.FindPeliculaId(id);
+        }
+
+        [HttpGet]
+        [Route("[action]/{idGenero}")]
+        public ActionResult<List<Pelicula>> FindPeliculasGenero(int idGenero) {
+
+            return this.repo.FindPeliculasGenero(idGenero);
+        }
+
+        [HttpGet]
+        [Route("[action]/{idNacionalidad}")]
+        public ActionResult<List<Pelicula>> FindPeliculasNacionalidad(int idNacionalidad) {
+
+            return this.repo.FindPeliculasNacionalidad(idNacionalidad);
+        }
+
+        [HttpGet]
+        [Route("[action]/{idgenero}/{idnacionalidad}")]
+        public ActionResult<List<Pelicula>> FindPeliculasGeneroNacionalidad(int idgenero, int idnacionalidad) {
+
+            return this.repo.FindPeliculasNacGen(idgenero, idnacionalidad);
+        }
+
+        [HttpPost]
+        public ActionResult InsertPelicula(Pelicula pelicula) {
+
+            this.repo.InsertPelicula(pelicula.IdDistribuidor,pelicula.IdGenero,pelicula.Titulo,pelicula.IdNacionalidad,
+                pelicula.Argumento,pelicula.Foto,pelicula.FechaEstreno,pelicula.Actores,pelicula.Duracion,pelicula.Precio);
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public ActionResult ModificarPelicula(Pelicula pelicula) {
+
+            this.repo.ModifyPelicula(pelicula.IdPelicula, pelicula.IdDistribuidor, pelicula.IdGenero, pelicula.Titulo, pelicula.IdNacionalidad,
+                pelicula.Argumento, pelicula.Foto, pelicula.FechaEstreno, pelicula.Actores, pelicula.Duracion, pelicula.Precio);
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult EliminarPelicula(int idPelicula) {
+
+            this.repo.DeletePelicula(idPelicula);
+
+            return Ok();
+        }
+    }
+}
---
+++ ApiPeliculas/ApiPeliculas/Models/Pelicula.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPeliculas.Models
+{
+    [Table("PELICULA")]
+    public class Pelicula
+    {
+        [Key]
+        [Column("idPelicula")]
+        public int IdPelicula { get; set; }
+
+        [Column("idDistribuidor")]
+        public int IdDistribuidor { get; set; }
+
+        [Column("idGenero")]
+        public int IdGenero { get; set; }
+
+        [Column("titulo")]
+        public string Titulo { get; set; }
+
+        [Column("idNacionalidad")]
+        public int IdNacionalidad { get; set; }
+
+        [Column("argumento")]
+        public string Argumento { get; set; }
+
+        [Column("foto")]
+        public string Foto { get; set; }
+
+        [Column("fechaEstreno")]
+        public string FechaEstreno { get; set; }
+
+        [Column("actores")]
+        public string Actores { get; set; }
+
+        [Column("duracion")]
+        public int Duracion { get; set; }
+
+        [Column("precio")]
+        public int Precio { get; set; }
+    }
+}
