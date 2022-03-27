2dac6d5bc291228f0971dc62f0029f8da2fbb2e2
MariaAdel-22 <maria.espana1@educa.madrid.org>
2022-03-27 20:13:44 +0200

    com1

---
+++ MvcPruebaSeguridadTokenAdopet/MvcPruebaSeguridadTokenAdopet/Controllers/PruebaController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcPruebaSeguridadTokenAdopet.Services;
+using NuGetAdoPet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcPruebaSeguridadTokenAdopet.Controllers
+{
+    public class PruebaController : Controller
+    {
+        private ServicePruebas service;
+
+        public PruebaController(ServicePruebas service)
+        {
+
+            this.service = service;
+        }
+
+        public IActionResult LogIn() {
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LogIn(string username, string password)
+        {
+
+            string token = await this.service.GetToken(username, password);
+
+            if (token == null)
+            {
+
+                ViewData["MENSAJE"] = "Usuario/Password incorrectos";
+
+                return View();
+            }
+            else
+            {
+                //UNA VEZ QUE TENEMOS EL TOKEN RECUPERAMOS EL PERFIL DEL EMPLEADO Y ALMACENAMOS LOS DATOS DE FORMA PERSONALIZADA
+                ViewData["MENSAJE"] = "Bienvenid@";
+
+                VistaCuentas cuenta = await this.service.GetCuentaAsync(token);
+
+                return View(cuenta);
+                //return RedirectToAction("Index", "Home");
+            }
+        }
+    }
+}
---
+++ MvcPruebaSeguridadTokenAdopet/MvcPruebaSeguridadTokenAdopet/Startup.cs
@@ -0,0 +1,80 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MvcPruebaSeguridadTokenAdopet.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcPruebaSeguridadTokenAdopet
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            string urlApi = this.Configuration.GetValue<string>("ApiAzure:cadenaapi");
+
+            ServicePruebas service = new ServicePruebas(urlApi);
+            services.AddTransient<ServicePruebas>(x => service);
+
+            services.AddDistributedMemoryCache();
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.IsEssential = true;
+            });
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            }).AddCookie();
+
+            services.AddControllersWithViews(op => op.EnableEndpointRouting = false);
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
