d637f050de7cd65c15a15fbf67382d9d5914da21
MariaAdel-22 <maria.espana1@educamadrid.org>
2022-03-31 13:17:27 +0200

    com3

---
+++ MvcCoreServiceBus/MvcCoreServiceBus/Controllers/ServiceBusController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreServiceBus.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreServiceBus.Controllers
+{
+    public class ServiceBusController : Controller
+    {
+        private ServiceQueueBus service;
+
+        public ServiceBusController(ServiceQueueBus service) {
+
+            this.service = service;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string mensaje, string accion)
+        {
+            if (accion.ToLower() == "enviar") {
+
+                await this.service.SendMessageAsync(mensaje);
+
+                ViewData["MENSAJE"] = "Mensaje enviado correctamente";
+
+            } else if (accion.ToLower() == "recibir") {
+
+                List<string> mensajes = await this.service.ReceiveMessageAsync();
+
+                ViewData["MENSAJE"] = "Mensajes recibidos " + mensajes.Count();
+
+                return View(mensajes);
+            }
+            return View();
+        }
+
+    }
+}
---
+++ MvcCoreServiceBus/MvcCoreServiceBus/Services/ServiceQueueBus.cs
@@ -0,0 +1,84 @@
+﻿using Azure.Messaging.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MvcCoreServiceBus.Services
+{
+    public class ServiceQueueBus
+    {
+        private ServiceBusClient client;
+        private List<string> mensajes;
+
+        public ServiceQueueBus(ServiceBusClient client) {
+
+            this.client = client;
+            this.mensajes = new List<string>();
+        }
+
+        //LOS PROCESOS DE RECEPCION DE MENSAJES SE REALIZAN DE FORMA ASINCRONA, SE UTILIZA UN METODO DELEGADO QUE IRA LEYENDO CADA
+        //MENSAJE
+
+        //METODO PARA ENVIAR MENSAJE
+        public async Task SendMessageAsync(string data) {
+
+            //PARA ENVIAR NECESITAMOS UN SENDER
+            ServiceBusSender sender = this.client.CreateSender("developers");
+
+            //EL OBJETO PARA ENVIAR MENSAJES ES MESSAGE
+            ServiceBusMessage message = new ServiceBusMessage(data);
+
+            await sender.SendMessageAsync(message);
+        }
+
+        //METODO PARA RECIBIR LOS MENSAJES. UTILIZA METODOS DELEGADOS PARA PROCESAR CADA LECTURA DE MENSAJE
+        public async Task<List<string>> ReceiveMessageAsync() {
+
+            ServiceBusProcessor processor = this.client.CreateProcessor("developers");
+
+            //EL PROCESO DE LECTURA SE DEBE REALIZAR EN OTROS METODOS, ES DECIR, RELLENAR LA LISTA DE MENSAJES SE HACE
+            //EN OTRO METODO. ESTE METODO LO QUE DEVUELVE SON LOS MENSAJES
+
+            //DELEGAR METODO DE LECTURA
+            processor.ProcessMessageAsync += Processor_ProcessMessageAsync;
+
+            //DELEGAR METODO POR EXCEPCIONES
+            processor.ProcessErrorAsync += Processor_ProcessErrorAsync;
+
+            //AQUI COMIENZA A LEER LA COLA DE MENSAJES
+            await processor.StartProcessingAsync();
+
+            Thread.Sleep(3000);
+
+            //AQUI TERMINA LA LECTURA DE MENSAJES
+            await processor.StopProcessingAsync();
+
+            //DEVOLVEMOS LOS MENSAJES LEIDOS EN EL METODO Processor_ProccesMessageAsync
+            return this.mensajes;
+        }
+
+        private async Task Processor_ProcessMessageAsync(ProcessMessageEventArgs arg)
+        {
+            //AQUI LEEMOS CADA MENSAJE Y DECIDIMOS QUE HACER
+
+            string content = arg.Message.Body.ToString();
+
+            //AÑADIMOS LOS MENSAJES A NUESTRA CLASE LIST
+            this.mensajes.Add(content);
+
+            //DEBEMOS INDICAR QUE HEMOS PROCESADO ESTE MENSAJE
+            await arg.CompleteMessageAsync(arg.Message);
+        }
+
+        private Task Processor_ProcessErrorAsync(ProcessErrorEventArgs arg)
+        {
+            //EMTRA EM CASO DE QUE DIERA UNA EXCEPCION AL PROCESAR LOS MENSAJES
+
+            Debug.WriteLine(arg.Exception.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
