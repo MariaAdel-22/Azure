d514bf9e72557666036e142a06f733b3116d09db
MariaAdel-22 <maria.espana1@educamadrid.org>
2022-04-04 14:06:51 +0200

    com1

---
+++ MvcPruebaMetodosApiAdoPetToken/MvcPruebaMetodosApiAdoPetToken/Controllers/PruebaController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MvcPruebaMetodosApiAdoPetToken.Services;
+using Newtonsoft.Json;
+using NuGetAdoPet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace MvcPruebaMetodosApiAdoPetToken.Controllers
+{
+    public class PruebaController : Controller
+    {
+        private ServiceAlumno service;
+
+        public PruebaController(ServiceAlumno service)
+        {
+
+            this.service = service;
+        }
+
+        public IActionResult LogIn()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LogIn(string nombre, string pass)
+        {
+            string token = await this.service.GetTokenAsync(nombre, pass);
+
+            if (token == null)
+            {
+                ViewData["MENSAJE"] = "Usuario/Password incorrectos";
+                return View();
+            }
+            else
+            {
+
+                VistaCuentas cuenta =await this.service.BuscarCuenta(token);
+
+                ClaimsIdentity identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme
+                    , ClaimTypes.Name, ClaimTypes.Role);
+
+                identity.AddClaim(new Claim("TOKEN", token));
+                identity.AddClaim(new Claim("CUENTA", JsonConvert.SerializeObject(cuenta)));
+
+                ClaimsPrincipal principal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync
+                    (CookieAuthenticationDefaults.AuthenticationScheme
+                    , principal, new AuthenticationProperties
+                    {
+                        IsPersistent = true,
+                        ExpiresUtc = DateTime.UtcNow.AddMinutes(30)
+                    });
+                return RedirectToAction("GetProtectoras");
+            }
+
+        }
+
+        public async Task<IActionResult> GetProtectoras() {
+
+            string token = HttpContext.User.FindFirst("TOKEN").Value;
+            VistaCuentas cuenta=JsonConvert.DeserializeObject<VistaCuentas>(HttpContext.User.FindFirst("CUENTA").Value);
+
+            List<Protectora> protectoras = new List<Protectora>();
+
+            if (cuenta.usuario != null) {
+
+                protectoras = await this.service.GetProtectoras(token, cuenta.usuario.Ciudad);
+
+            } else if (cuenta.protectora != null) {
+
+                protectoras = await this.service.GetProtectoras(token, cuenta.protectora.Ciudad);
+            }
+
+            return View(protectoras);
+        }
+
+        public async Task<IActionResult> AnimalesProtectora() {
+
+            string token = HttpContext.User.FindFirst("TOKEN").Value;
+            VistaCuentas cuenta = JsonConvert.DeserializeObject<VistaCuentas>(HttpContext.User.FindFirst("CUENTA").Value);
+
+            List<Animal> animales;
+
+            if (cuenta.protectora != null)
+            {
+
+                animales = await this.service.GetAnimalesProtectora(cuenta.protectora.IdProtectora, token);
+            }
+            else {
+
+                animales = new List<Animal>();
+            }
+            
+
+            return View(animales);
+        }
+
+        public IActionResult InsertarAnimal() {
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> InsertarAnimal(string Nombre,string Edad,string Especie,string Genero,IFormFile Imagen,string Peso) {
+
+            string token = HttpContext.User.FindFirst("TOKEN").Value;
+            await this.service.InsertarAnimal(Nombre,Edad,Especie,Genero,Imagen.FileName,Peso,token);
+
+            return RedirectToAction("AnimalesProtectora");
+        }
+    }
+}
---
+++ MvcPruebaMetodosApiAdoPetToken/MvcPruebaMetodosApiAdoPetToken/Services/ServiceAlumno.cs
@@ -0,0 +1,171 @@
+﻿using Microsoft.AspNetCore.Http;
+using MvcPruebaMetodosApiAdoPetToken.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NuGetAdoPet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcPruebaMetodosApiAdoPetToken.Services
+{
+    public class ServiceAlumno
+    {
+        private string UrlApi;
+        private MediaTypeWithQualityHeaderValue Header;
+
+        public ServiceAlumno(string urlapi)
+        {
+            this.UrlApi = urlapi;
+            this.Header = new MediaTypeWithQualityHeaderValue("application/json");
+        }
+
+        public async Task<string> GetTokenAsync(string nombre, string apellidos)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(this.UrlApi);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                LoginModel model = new LoginModel
+                {
+                    Nombre=nombre,
+                    Password=apellidos
+                };
+                string json = JsonConvert.SerializeObject(model);
+                StringContent content =
+                    new StringContent(json, Encoding.UTF8, "application/json");
+                string request = "/api/inicio/validarcuenta";
+                HttpResponseMessage response =
+                    await client.PostAsync(request, content);
+                if (response.IsSuccessStatusCode)
+                {
+                    string data = await response.Content.ReadAsStringAsync();
+                    JObject jObject = JObject.Parse(data);
+                    string token = jObject.GetValue("response").ToString();
+                    return token;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        private async Task<T> CallApiAsync<T>(string request)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(this.UrlApi);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                HttpResponseMessage response =
+                    await client.GetAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    T data = await response.Content.ReadAsAsync<T>();
+                    return data;
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+        }
+
+        private async Task<T> CallApiAsync<T>(string request, string token)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(this.UrlApi);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                client.DefaultRequestHeaders.Add("Authorization", "bearer " + token);
+                HttpResponseMessage response =
+                    await client.GetAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    T data = await response.Content.ReadAsAsync<T>();
+                    return data;
+                }
+                else
+                {
+                    return default(T);
+                }
+
+            }
+        }
+
+        public async Task<VistaCuentas> BuscarCuenta(string token) {
+
+            string request = "/api/inicio/buscarcuenta";
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(this.UrlApi);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                client.DefaultRequestHeaders.Add("Authorization", "bearer " + token);
+
+                HttpResponseMessage response =await client.GetAsync(request);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string cuen = await response.Content.ReadAsStringAsync();
+
+                    VistaCuentas cuenta = JsonConvert.DeserializeObject<VistaCuentas>(cuen);
+
+                    return cuenta;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public async Task<List<Protectora>> GetProtectoras(string token,string ciudad) {
+
+            string request = "/api/protectoras/getprotectoras/"+ciudad;
+
+           List<Protectora>protectoras=await this.CallApiAsync<List<Protectora>>(request, token);
+
+            return protectoras;
+        }
+
+        public async Task<List<Animal>> GetAnimalesProtectora(int codigoProtectora,string token) {
+
+            string request = "/api/protectoras/getanimalesprotectora/" + codigoProtectora;
+
+            List<Animal> anim = await this.CallApiAsync<List<Animal>>(request, token);
+
+            return anim;
+        }
+
+        public async Task InsertarAnimal(string nombre,string edad,string especie,string genero,string imagen,string peso,string token) {
+
+            using (HttpClient client = new HttpClient())
+            {
+
+                client.BaseAddress = new Uri(this.UrlApi);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                client.DefaultRequestHeaders.Add("Authorization", "bearer " + token);
+
+                string request = "/api/protectoras/insertaranimal";
+
+                Animal an = new Animal { Nombre=nombre, Edad = edad, Especie = especie, Genero = genero, Imagen = imagen, Peso = peso };
+
+                string json = JsonConvert.SerializeObject(an);
+
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync(request, content);
+            }
+        }
+    }
+}
